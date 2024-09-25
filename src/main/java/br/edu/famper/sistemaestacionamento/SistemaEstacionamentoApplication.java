b175a8265fcbb8e9d4437922728ab032295fbf5d
Welitonherdt <welitonherdt2001@gmail.com>
2024-09-25 20:00:43 -0300

    Initial commit

---
+++ src/main/java/br/edu/famper/sistemaestacionamento/SistemaEstacionamentoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.famper.sistemaestacionamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEstacionamentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaEstacionamentoApplication.class, args);
+    }
+
+}
